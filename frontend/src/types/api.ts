fd79ff00d1e3b61f91523755e0f5b6c2c55bbb67
nguyenhung2203 <nguyenhung22032006@gmail.com>
2026-04-07 17:24:27 +0700

    fix lại backend

--- frontend/src/types/api.ts
+++ frontend/src/types/api.ts
@@ -184,7 +184,7 @@ export interface AdminDashboardDashboard {
     suspiciousClicks: number
     errorRate: number
     queueLagSeconds: number
-    systemUptimeDays: number
+    redirectLatencyP95Ms: number
   }
 }
 
